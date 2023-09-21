bbee727201c6dc82b892f5043d84bef79d1a9fe2
pavlos <pavlosn.pn@gmail.com>
2023-09-21 18:56:51 +0300

    finished section 51

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const User = require('../models/user');
+const { storeReturnTo } = require('../middleware');
+
 
 router.get("/register", (req, res) => {
     res.render("users/register");
@@ -13,22 +15,27 @@ router.post("/register", catchAsync(async (req, res) => {
         const { username, email, password } = req.body;
         const newUser = new User({ username, email });
         const newUserRegistered = await User.register(newUser, password);
-        req.flash("success", "Welcome to Yelpcamp!");
-        res.redirect("/campgrounds");
-        
+        req.login(newUserRegistered, err => {
+            if (err) return next(err)
+            req.flash("success", "Welcome to Yelpcamp!");
+            res.redirect("/campgrounds");
+        });
+
+
     } catch (error) {
         req.flash("error", error.message)
         res.redirect("register");
     }
 }));
 
-router.get("/login",catchAsync(async(req,res)=>{
-     res.render("users/login")
+router.get("/login", catchAsync(async (req, res) => {
+    res.render("users/login")
 }));
 
-router.post("/login",passport.authenticate("local",{failureFlash : true, failureRedirect: "/login"}),catchAsync(async(req,res)=>{
+router.post("/login", storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), catchAsync(async (req, res) => {
     req.flash("success", "welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = res.locals.returnTo || '/campgrounds'; // 
+    res.redirect(redirectUrl);
 }));
 
 router.get('/logout', (req, res, next) => {
@@ -39,6 +46,6 @@ router.get('/logout', (req, res, next) => {
         req.flash('success', 'Goodbye!');
         res.redirect('/campgrounds');
     });
-}); 
+});
 
 module.exports = router;
